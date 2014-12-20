77b6cf1795ceb5317198c1830426881724565358
ゆきかご <p.u.r.u.h.i.m.e@gmail.com>
2014-12-20 17:23:10 +0900

    Modelコンストラクタ：作成

---
+++ jasmine-standalone-2.1.3/spec/ModelSpec.js
@@ -0,0 +1,61 @@
+describe('Model', function(){
+    it('関数からの生成', function(){
+        var model = marnie.Model(0);
+        expect(model.data).toBe(0);
+
+        model.data = 10;
+        expect(model.data).toBe(0);
+    });
+
+    it('コンストラクタからの生成', function(){
+        var model = new marnie.Model("qwerty");
+        expect(model.data).toBe("qwerty");
+
+        model.data = "azerty";
+        expect(model.data).toBe("qwerty");
+    });
+
+    it('起動時に関数を指定', function(){
+        var model = new marnie.Model(function(){
+            return "create";
+        });
+        expect(model.data).toBe("create");
+    });
+
+    it('起動時にオブジェクトを指定', function(){
+        var model = new marnie.Model({
+            hoge:{
+                hufa:{}
+            }
+        });
+
+        expect(model.data).toEqual({
+            hoge:{
+                hufa:{}
+            }
+        });
+
+        model.data.hoge.hufa = 20;
+        expect(model.data.hoge.hufa).toEqual({});
+    });
+
+    describe('nextLayer()', function(){
+        it('ごろごろごろごろ', function(){
+            var model = marnie.Model(0);
+
+            model.nextLayer(function(data, layer){
+                expect(data).toBe(0);
+
+                layer.layer1 = true;
+            }).nextLayer(function(data, layer){
+                expect(data).toBe(0);
+                expect(layer.layer1).toEqual(true);
+
+                layer.layer1 = false;
+            }).nextLayer(function(data, layer){
+                expect(layer.layer1).toBeFalsy();
+                expect(Object.getPrototypeOf(Object.getPrototypeOf(layer)).layer1).toBeTruthy();
+            });
+        });
+    });
+});
--- marnie.js
+++ marnie.js
@@ -1,106 +1,7 @@
-(function(marnie){
-    /**
-     * とある範囲を指定します。
-     * このクラスはイミュータブルです。
-     * @param first
-     * @param last
-     * @param forEachCallback
-     * @returns {marnie.Range}
-     * @constructor
-     */
-    marnie.Range = function(first, last, forEachCallback){
-        if(typeof first !== 'number' || typeof last !== 'number'){
-            console.log('数値を指定してください');
-            return;
-        }
-        if(this.constructor != marnie.Range){
-            return new marnie.Range(first, last, forEachCallback);
-        }
-
-        if(first < last){
-            this.FIRST = first;
-            this.LAST = last;
-        }else{
-            var swap = first;
-            this.FIRST = last;
-            this.LAST = swap;
-        }
-
-        Object.freeze(this);
-
-        if(typeof forEachCallback === 'function') this.forEach(forEachCallback);
-    };
-
-    //デフォルト値
-    marnie.Range.prototype.FIRST = 0;
-    marnie.Range.prototype.LAST = 0;
-
-    /**
-     * 範囲内の値を返します。
-     * @param callback
-     */
-    marnie.Range.prototype.forEach = function(callback){
-        if(typeof callback !== 'function'){
-            console.log('関数を指定してください');
-            return;
-        }
-
-        var index = 0;
-
-        for(var count = this.FIRST ; count <= this.LAST ; count++, index++){
-            callback(count, index);
-        }
-    };
-
-    /**
-     * 範囲内である場合、trueを返します。
-     * @param check
-     * @returns {boolean}
-     */
-    marnie.Range.prototype.on = function(check){
-        return this.FIRST <= check && check <= this.LAST;
-    }
-
-    /**
-     * 範囲外である場合、trueを返します。
-     * @param check
-     * @returns {boolean}
-     */
-    marnie.Range.prototype.out = function(check){
-        return check < this.FIRST || this.LAST < check;
-    }
-
-    /**
-     * 範囲内か範囲外を判定し、コールバックを実行します。
-     * @param check
-     */
-    marnie.Range.prototype.route = function(check){
-        //routeはcheckの値毎にインスタンスを生成する必要がある。
-        //thisと関数を紐付けるとメモリリークになりそうなので可能な限り値を
-        //オブジェクト内に収めるようにした。
-        //
-        //第二引数に{on:func, out:func}とする手もあるが、これだと複数の関数が同時に実行出来ない。
-        //また、on()out()が分かれている為、onだけ実行したいという場合にも一応対応出来る。
-        return {
-            _CHECK:check,
-            FIRST:this.FIRST,
-            LAST:this.LAST,
-            on:function(callback){
-                check = null;
-                if(marnie.Range.prototype.on.call(this, this._CHECK)){
-                    callback();
-                }
-                return this;
-            },
-            out:function(callback){
-                check = null;
-                if(marnie.Range.prototype.out.call(this, this._CHECK)){
-                    callback();
-                }
-                return this;
-            }
-        };
-    };
+(function(marnie, modules){
+    modules.forEach(function(module){
+        module(marnie);
+    });
 })((function(){
     //init
     if('marnie' in this){
@@ -111,4 +12,179 @@
     }
 
     return marnie;
-})());
+})(), [
+    function(marnie){
+        /**
+         * とある範囲を指定します。
+         * このクラスはイミュータブルです。
+         * @param first
+         * @param last
+         * @param forEachCallback
+         * @returns {marnie.Range}
+         * @constructor
+         */
+        marnie.Range = function(first, last, forEachCallback){
+            if(typeof first !== 'number' || typeof last !== 'number'){
+                console.log('数値を指定してください');
+                return;
+            }
+            if(this.constructor != marnie.Range){
+                return new marnie.Range(first, last, forEachCallback);
+            }
+
+            if(first < last){
+                this.FIRST = first;
+                this.LAST = last;
+            }else{
+                var swap = first;
+                this.FIRST = last;
+                this.LAST = swap;
+            }
+
+            Object.freeze(this);
+
+            if(typeof forEachCallback === 'function') this.forEach(forEachCallback);
+        };
+
+        //デフォルト値
+        marnie.Range.prototype.FIRST = 0;
+        marnie.Range.prototype.LAST = 0;
+
+        /**
+         * 範囲内の値を返します。
+         * @param callback
+         */
+        marnie.Range.prototype.forEach = function(callback){
+            if(typeof callback !== 'function'){
+                console.log('関数を指定してください');
+                return;
+            }
+
+            var index = 0;
+
+            for(var count = this.FIRST ; count <= this.LAST ; count++, index++){
+                callback(count, index);
+            }
+        };
+
+        /**
+         * 範囲内である場合、trueを返します。
+         * @param check
+         * @returns {boolean}
+         */
+        marnie.Range.prototype.on = function(check){
+            return this.FIRST <= check && check <= this.LAST;
+        }
+
+        /**
+         * 範囲外である場合、trueを返します。
+         * @param check
+         * @returns {boolean}
+         */
+        marnie.Range.prototype.out = function(check){
+            return check < this.FIRST || this.LAST < check;
+        }
+
+        /**
+         * 範囲内か範囲外を判定し、コールバックを実行します。
+         * @param check
+         */
+        marnie.Range.prototype.route = function(check){
+            //routeはcheckの値毎にインスタンスを生成する必要がある。
+            //thisと関数を紐付けるとメモリリークになりそうなので可能な限り値を
+            //オブジェクト内に収めるようにした。
+            //
+            //第二引数に{on:func, out:func}とする手もあるが、これだと複数の関数が同時に実行出来ない。
+            //また、on()out()が分かれている為、onだけ実行したいという場合にも一応対応出来る。
+            return {
+                _CHECK:check,
+                FIRST:this.FIRST,
+                LAST:this.LAST,
+                on:function(callback){
+                    check = null;
+                    if(marnie.Range.prototype.on.call(this, this._CHECK)){
+                        callback();
+                    }
+                    return this;
+                },
+                out:function(callback){
+                    check = null;
+                    if(marnie.Range.prototype.out.call(this, this._CHECK)){
+                        callback();
+                    }
+                    return this;
+                }
+            };
+        };
+    },
+    function(marnie){
+        /**
+         * Modelを表現します。
+         * @param firstData
+         * @returns {Model} このオブジェクトはイミュータブルです。
+         * @constructor
+         */
+        marnie.Model = function(firstData){
+            if(this.constructor !== marnie.Model){
+                return new marnie.Model(firstData);
+            }
+
+            var lastData;
+            if(typeof firstData === 'function'){
+                lastData = firstData();
+            }else if(typeof firstData === 'object'){
+                lastData = firstData;
+                this._mahyado(lastData);
+            }else{
+                lastData = firstData;
+            }
+
+            this.data = lastData;
+
+            Object.freeze(this);
+        };
+        marnie.Model.prototype._mahyado = function(object){
+            if(typeof object === 'object' && Object.isFrozen(object) === false){
+                Object.freeze(object);
+                Object.keys(object).forEach(function(key){
+                    this._mahyado(object[key]);
+                }, this);
+            }
+        };
+        /**
+         * データに対して必要な処理を行います。
+         *
+         * nextLayer()の目的は、データと処理を分離する事です。
+         * ここで言う処理はMVCにおけるどの層にも当てはまります。
+         * nextLayer()のコールバックはビジネスロジックかもしれないし、Viewかもしれないし、Controllerかもしれません。
+         * どのような動作をすべきかはユーザが決定すべき事柄です。
+         *
+         * @param callback(data, layer)
+         *              data (*):      this.dataです
+         *              layer(object): プロトタイプチェーンされたオブジェクトを返します。
+         *                             nextが呼ばれると、前のオブジェクトをプロトタイプに入れた新しい空のオブジェクトを生成し、layerに設定します。
+         * @returns {{nextLayer: Function}} このメソッドは状態を持つオブジェクトを生成します。
+         */
+        marnie.Model.prototype.nextLayer = function(callback){
+            var self = this;
+            var Layer = function(){};
+            var layer = new Layer;
+
+            callback(this.data, layer);
+            //freezeするとプロトタイプチェーンのいっこ上のプロパティが更新出来なくなる。
+            //Object.freeze(layer);
+
+            return {
+                nextLayer:function(callback){
+                    Layer.prototype = layer;
+                    layer = new Layer;
+
+                    callback(self.data, layer);
+                    //Object.freeze(layer);
+
+                    return this;
+                }
+            };
+        };
+    }
+]);
